3de4a54f098a0f63d8bd3b2db0dc70cac226fafd
Tozinoo <goldboy122@gmail.com>
2024-02-24 23:38:36 +0900

    feat : slice ipv4 header, tcp header

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 use std::io;
+use etherparse::ip_number::TCP;
+
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x0800 {
-            // no ipv4
-            continue
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // not ipv4
+            continue;
         }
-        eprintln!("read {} bytes: (flags: {:x}, proto: {:x}, {:x?}", nbytes - 4 ,flags, proto, &buf[4..nbytes]);
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                if proto != TCP {
+                    // not tcp
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4+p.slice().len()..]) {
+                    Ok(p) => {
+                        // eprintln!("PONG {} ({}) from {} tun0: {} bytes of data.", src, src, dst,length );
+                        eprintln!("{} -> {} {}b of tcp to port {} ", src, dst,p.slice().len(),p.destination_port());
+                    },
+                    Err(e) =>{
+                        eprintln!("ignoring {:?}", e);
+                    }
+                }
+
+
+            },
+            Err(e) =>{
+                eprintln!("ignoring weird packet {:?}", e);
+            }
+
+        }
+
     }
     // Ok(())
 }
