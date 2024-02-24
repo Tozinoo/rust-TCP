550f447f7b92bc4f2d8dbd75fdc6d3a7447fe2bd
Tozinoo <goldboy122@gmail.com>
2024-02-24 22:06:56 +0900

    feat : catrgorize a protocol

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use std::io;
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x?}", nbytes, &[..nbytes]);
-    Ok(())
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if proto != 0x0800 {
+            // no ipv4
+            continue
+        }
+        eprintln!("read {} bytes: (flags: {:x}, proto: {:x}, {:x?}", nbytes - 4 ,flags, proto, &buf[4..nbytes]);
+    }
+    // Ok(())
 }
